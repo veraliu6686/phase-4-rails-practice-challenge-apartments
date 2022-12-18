a86acba7e1a8f46a875120ef29eb0802378e7c75
Lianzi <lianziliu.sugar@gmail.com>
2022-12-17 21:09:48 -0500

    done

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+  resources :leases, only: [:create, :destroy]
+  resources :tenants, only: [:index, :update, :create, :destroy]
+  resources :apartments, only: [:index, :update, :create, :destroy]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
